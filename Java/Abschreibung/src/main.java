3ab041b6dde5bbd90248c980a07fde6357e42ced
Jonas_Heuberger <90388219+Jonas-Heuberger@users.noreply.github.com>
2021-11-17 20:59:53 +0100

    update

--- Java/Abschreibung/src/main.java
+++ Java/Abschreibung/src/main.java
@@ -23,7 +23,7 @@ public class main {
         double result3 = 0;
         double result = Betrag / 100;
         double result2 = result * Prozentsatz;
-       while (Jahre >= 0){
+       while (Jahre >= 1){
            Jahre--;
            result3 = Betrag - result2;
        }
