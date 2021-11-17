b016d406ec204df030fff8a22370b66c50e13c7b
Jonas_Heuberger <90388219+Jonas-Heuberger@users.noreply.github.com>
2021-11-18 00:30:40 +0100

    update

--- Java/Tic-Tac-Toe/src/Game.java
+++ Java/Tic-Tac-Toe/src/Game.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
+
 public class Game {
+    int PlayerOne = 0;
+    int PlayerTwo = 1;
+
+    public static int Abfrage(){
+        System.out.println("Wo wollen sie ihr Symbol Plazieren? ");
+        Scanner scan = new Scanner(System.in);
+        int Zahl = scan.nextInt();
+        return Zahl;
+    }
+
+
 
 }
--- Java/Tic-Tac-Toe/src/main.java
+++ Java/Tic-Tac-Toe/src/main.java
@@ -1,5 +1,18 @@
 public class main {
     public static void main(String[] args) {
+        System.out.println(" 0 | 1 | 2\n" +
+                " ---------\n" +
+                " 3 | 4 | 5\n" +
+                " ---------\n" +
+                " 6 | 7 | 8");
+        double Position = Game.Abfrage();
+
+        if (Position == 0){
+            Position = Grid.twoD_arr[0][0];
+        }
+        if (Position == 1){
+            Position = twoD_arr[][]
+        }
 
     }
 }
