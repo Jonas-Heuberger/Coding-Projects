2486113ae2f88a02014bfb94e8d1a851f9d1637c
Jonas_Heuberger <90388219+Jonas-Heuberger@users.noreply.github.com>
2021-11-23 10:07:53 +0100

    update

---
+++ Java/Hund/src/Hund.java
@@ -0,0 +1,17 @@
+// Code from Jonas Keil
+
+public class Hund {
+    // Attribute
+    int alter;
+    String art;
+    String fellfarbe;
+    boolean kannPfoteGeben;
+
+    //Methoden
+    public void bellen(){
+        System.out.println("Wuff Wuff");
+    }
+    public void plusRechnen(int num1, int num2, int num3){
+        System.out.println(num1 + num2 + num3);
+    }
+}
---
+++ Java/Hund/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Hund hundObject1 = new Hund();
+        hundObject1.bellen();
+        hundObject1.plusRechnen(1,3, 4);
+    }
+}
