d08e7f265d6c5411f8fd08505d01f7709e9d0c7d
Jonas_Heuberger <90388219+Jonas-Heuberger@users.noreply.github.com>
2021-11-30 10:14:25 +0100

    update

--- Java/Katze/src/main.java
+++ Java/Katze/src/main.java
@@ -1,6 +1,9 @@
 public class main {
     public static void main(String[] args) {
-        Katze cat = new Katze(4, "braun", "blau", "tieger", 40);
+        Katze cat = new Katze(4, "braun", "blau", "tiger", 40);
         cat.miauen("miau");
+        Katze[] array = new Katze[1];
+        array[0] = cat;
+        System.out.println(array[0].alter);
     }
 }
