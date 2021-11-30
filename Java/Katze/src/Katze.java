d384ff1365eabb3e03a7b63094d9e129e84d0e16
Jonas_Heuberger <90388219+Jonas-Heuberger@users.noreply.github.com>
2021-11-30 09:14:25 +0100

    update

---
+++ Java/Katze/src/Katze.java
@@ -0,0 +1,22 @@
+public class Katze {
+    //Attribute
+    int alter;
+    String fellfarbe;
+    String augenfarbe;
+    String rasse;
+    int grösse;
+
+    //Konstruktor
+    public Katze(int alter, String fellfarbe, String augenfarbe, String rasse, int grösse){
+        this.alter = alter;
+        this.fellfarbe = fellfarbe;
+        this.augenfarbe = augenfarbe;
+        this.rasse = rasse;
+        this.grösse = grösse;
+    }
+    //Methoden
+
+    public void miauen(String miauen){
+        System.out.println(miauen);
+    }
+}
---
+++ Java/Katze/src/main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+        Katze cat = new Katze(4, "braun", "blau", "tieger", 40);
+        cat.miauen("miau");
+    }
+}
