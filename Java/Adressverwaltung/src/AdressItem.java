5429d89adb7a66ee07a5457c333026ff15bb1b81
Jonas_Heuberger <90388219+Jonas-Heuberger@users.noreply.github.com>
2021-12-09 08:53:53 +0100

    update

---
+++ Java/Adressverwaltung/src/AdressItem.java
@@ -0,0 +1,19 @@
+public class AdressItem {
+    //Attribute
+
+    int id;
+    String nachname;
+    String vorname;
+    String strasse;
+    int plz;
+    String ort;
+
+    public AdressItem(int id, String nachname, String vorname, String strasse, int plz, String ort) {
+        this.id = id;
+        this.nachname = nachname;
+        this.vorname = vorname;
+        this.strasse = strasse;
+        this.plz = plz;
+        this.ort = ort;
+    }
+}
--- Java/Adressverwaltung/src/Main.java
+++ Java/Adressverwaltung/src/Main.java
@@ -1,5 +1,51 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        AdressItem userAdresse = AdressItem();
+
+        System.out.println("id: " + userAdresse.id);
+        System.out.println("Nachname: " + userAdresse.nachname);
+        System.out.println("Vorname: " + userAdresse.vorname);
+        System.out.println("Strasse: " + userAdresse.strasse);
+        System.out.println("PLZ: " + userAdresse.plz);
+        System.out.println("Ort: " + userAdresse.ort);
     }
+
+     private static int AbfrageINT(){
+        Scanner scanner = new Scanner(System.in);
+        int zahl = scanner.nextInt();
+        return zahl;
+    }
+
+    private static String AbrageString(){
+        Scanner scanner = new Scanner(System.in);
+        String txt = scanner.next();
+        return txt;
+    }
+
+    private static AdressItem AdressItem(){
+        System.out.println("geben Sie Ihre ID ein: ");
+        int userId = AbfrageINT();
+
+        System.out.println("geben Sie Ihren Nachname ein: ");
+        String userNachname = AbrageString();
+
+        System.out.println("geben Sie Ihren Vornamen ein: ");
+        String userVorname = AbrageString();
+
+        System.out.println("geben Sie Ihre Strasse ein: ");
+        String userStrasse = AbrageString();
+
+        System.out.println("geben Sie Ihre PLZ ein: ");
+        int userPLZ = AbfrageINT();
+
+        System.out.println("geben Sie Ihren Ort ein: ");
+        String userOrt = AbrageString();
+
+        AdressItem userAdresse = new AdressItem(userId, userNachname, userVorname, userStrasse, userPLZ, userOrt);
+        return userAdresse;
+    }
+
 }
