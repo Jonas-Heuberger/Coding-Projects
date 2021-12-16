79eddba04aa763b08844db1f4d07cfbcbb0dced4
Jonas_Heuberger <90388219+Jonas-Heuberger@users.noreply.github.com>
2021-12-16 10:24:55 +0100

    Update Main.java

--- Java/Lohnabrechnung/src/Main.java
+++ Java/Lohnabrechnung/src/Main.java
@@ -6,32 +6,22 @@ public class Main {
         System.out.println("Lohn: ");
         double lohn = abfrageDouble();
 
-        if (lohn > 12350){
-            ALV2(lohn);
-        }
-
         double AHVprozent = 5.275;
 
         double ALVprozent = 1.1;
 
-        double AHVbeitrag = AHVbeitrag(lohn, AHVprozent);
-
-        double ALVbeitrag = ALVbeitrag(lohn, ALVprozent);
-
-        double ALV2beitrag = ALV2(lohn);
-
-        System.out.println("Sie müssen " + AHVbeitrag + "CHF AHV bezahlen");
-
-        System.out.println("Sie müssen " + ALVbeitrag + "CHF ALV bezahlen");
-
-        System.out.println("Sie müssen " + ALV2beitrag + "CHF ALV2 bezahlen");
+        double ALV2limit = 12350;
 
 
+        if (lohn > 12350){   
+        AHVbeitrag(lohn, AHVprozent);
+        ALVbeitrag(lohn, ALVprozent, ALV2limit);
+        ALV2(lohn);
 
-
-
-
-
+        } else {
+            AHVbeitrag(lohn, AHVprozent);
+            ALVbeitrag(lohn, ALVprozent, ALV2limit);
+        }
     }
 
      private static double abfrageDouble(){
@@ -39,19 +29,24 @@ public class Main {
         double Zahl = scanner.nextDouble();
         return Zahl;
     }
-    private static double AHVbeitrag(double lohn, double AHVprozent){
-        return lohn / 100 * AHVprozent;
+    private static void AHVbeitrag(double lohn, double AHVprozent){
+        System.out.println((lohn / 100) * AHVprozent);
     }
 
-    private static double ALVbeitrag(double lohn, double ALVprozent){
-        return lohn / 100 * ALVprozent;
+    private static void ALVbeitrag(double lohn, double ALVprozent, double ALV2limit){
+        if (lohn > 12350) {
+           System.out.println((ALV2limit / 100) * ALVprozent);
+        } else {
+             System.out.println(lohn / 100 * ALVprozent);
+        }
+        
     }
 
-    static double ALV2(double lohn){
+    static void ALV2(double lohn){
             double zwischenSumme = lohn - 12350;
-            lohn = zwischenSumme / 100 * 0.5;
-            return lohn;
+            lohn = (zwischenSumme / 100) * 0.5;
+        System.out.println(lohn);
 
         }
     }
-}
+
