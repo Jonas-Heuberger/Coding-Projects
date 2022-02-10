56fb6aa7c9a0d5dffd776d171df2d5eb0a75db97
Jonas_Heuberger <90388219+Jonas-Heuberger@users.noreply.github.com>
2022-02-10 12:19:46 +0100

    update

--- Java/Airline/src/airline/Person.java
+++ Java/Airline/src/airline/Person.java
@@ -2,7 +2,7 @@ package airline;
 
 import java.io.Serializable;
 
-public class Person implements Serializable {
+public abstract class Person implements Serializable {
     String vorname;
     String nachname;
     int flugnummer;
--- Java/Airline/src/airline/Verwaltung.java
+++ Java/Airline/src/airline/Verwaltung.java
@@ -8,8 +8,48 @@ import java.util.Scanner;
 
 public class Verwaltung implements Serializable {
 
+    // enum für Rolle (Pilot etc)
+
 private List<Passagier> passagierList = new ArrayList<>();
 private List<CrewMember> crewMemberList = new ArrayList<>();
+
+    private void add(){
+
+    }
+
+    private void edit(){
+
+    }
+
+    private void show(){
+
+    }
+
+    private void delete(){
+
+    }
+
+    public void userInterface(){
+        System.out.println("Willkommen");
+        System.out.println("1: Hinzufügen");
+        System.out.println("2: Bearbeiten");
+        System.out.println("3: Anzeigen");
+        System.out.println("4: Löschen");
+        System.out.println("5: aelter als ein Jahr");
+
+        int userInput = abfrageINT();
+
+        switch (userInput){
+            case 1-> add();
+            case 2-> edit();
+            case 3-> show();
+            case 4-> delete();
+            default -> {
+                System.out.println("ungültiger Input");
+                userInterface();
+            }
+        }
+    }
 
 
     private static int abfrageINT(){
