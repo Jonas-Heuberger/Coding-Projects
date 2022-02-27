ef5bc9659eb5c39e978f90b50d7365b20324ad22
Jonas_Heuberger <90388219+Jonas-Heuberger@users.noreply.github.com>
2022-02-27 23:09:53 +0100

    created C# project and created my first function

---
+++ C#/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Print();
+        }
+
+        static void Print()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
