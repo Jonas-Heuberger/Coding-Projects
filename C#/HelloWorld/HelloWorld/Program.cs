805fe298d8a8b3df2bc0b2c1077934bff05b52b3
Jonas_Heuberger <90388219+Jonas-Heuberger@users.noreply.github.com>
2022-03-30 23:19:16 +0200

    played with some classes and objects

--- C#/HelloWorld/HelloWorld/Program.cs
+++ C#/HelloWorld/HelloWorld/Program.cs
@@ -2,17 +2,34 @@ using System;
 
 namespace HelloWorld
 {
-
 	public class main
 	{
-		 static void Main(string[] args)
+		static void Main(string[] args)
 		{
+			Cat cat = new Cat(12, 50, 60, "Albert", "tiger");
+			Console.WriteLine(cat.Name);
+			Console.WriteLine(cat.Age);
+			
 
-			int num = 15;
-			string result = (num >= 10) ? "big one" : "too low";
-			Console.WriteLine(result);
 		}
-		
-		
 	}
+
+	public class Cat
+    {
+		public int Age;
+		public double Height;
+		public double Weight;
+		public string Name;
+		public string Race;
+
+		public Cat(int Age, double Height, double Weight, string Name, string Race)
+        {
+			this.Age = Age;
+			this.Height = Height;
+			this.Weight = Weight;
+			this.Name = Name;
+			this.Race = Race;
+        }
+
+    }
 }
