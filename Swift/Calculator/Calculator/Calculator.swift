f8b4a41a3e339e0911b683a3e32e6e75a7e435ff
Jonas_Heuberger <90388219+Jonas-Heuberger@users.noreply.github.com>
2022-03-31 11:29:17 +0200

    tried some more stuff with swift

--- Swift/Calculator/Calculator/Calculator.swift
+++ Swift/Calculator/Calculator/Calculator.swift
@@ -10,21 +10,26 @@ import UIKit
 class Calculator: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.gray;
+        view.backgroundColor = UIColor.black;
         
-        let displayNum = UILabel(frame: CGRect(x:70, y:70, width: view.frame.size.width - 140, height: 50))
+        // UI view
+        let uiView = UIView(frame: CGRect(x: 50,
+                                          y: 50,
+                                          width: view.frame.size.width - 100,
+                                          height: view.frame.size.height - 100))
         
-        displayNum.backgroundColor = UIColor.black
-        displayNum.font = UIFont(name: "Arial", size: 28)
-        displayNum.textColor = UIColor.white
-        displayNum.textAlignment = NSTextAlignment.center
-        displayNum.text = "label"
-        displayNum.adjustsFontSizeToFitWidth = true
+        view.addSubview(uiView)
         
-        view.addSubview(displayNum);
+        let displayOutput = UILabel(frame: CGRect(x:70, y:70, width: view.frame.size.width - 140, height: 50))
+        displayOutput.backgroundColor = UIColor.gray
+        displayOutput.font = UIFont(name: "Arial", size: 28)
+        displayOutput.textColor = UIColor.white
+        displayOutput.textAlignment = NSTextAlignment.center
+        displayOutput.text = "Calculator"
+        displayOutput.adjustsFontSizeToFitWidth = true
+        view.addSubview(displayOutput)
         
         
-    }
-    
+            }
 }
 
