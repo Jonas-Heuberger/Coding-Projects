2f70fb7d5a8fab64acdcb02cd63e3be8310841f8
Jonas_Heuberger <90388219+Jonas-Heuberger@users.noreply.github.com>
2022-04-08 10:04:17 +0200

    added set and get methods to my Cat class

--- C#/HelloWorld/HelloWorld/Program.cs
+++ C#/HelloWorld/HelloWorld/Program.cs
@@ -7,29 +7,31 @@ namespace HelloWorld
 		static void Main(string[] args)
 		{
 			Cat cat = new Cat(12, 50, 60, "Albert", "tiger");
-			Console.WriteLine(cat.Name);
+			Console.WriteLine(cat.Name); 
 			Console.WriteLine(cat.Age);
+			Console.WriteLine(cat.Race);
+			Console.WriteLine(cat.Height);
+			Console.WriteLine(cat.Weight);
 			
 
 		}
 	}
 
 	public class Cat
-    {
-		public int Age;
-		public double Height;
-		public double Weight;
-		public string Name;
-		public string Race;
+	{
+		private int Age {get; set;}
+		public double Height { get { return Height; } set { Height = value; }}
+		public double Weight { get { return Weight; } set { Weight = value; }}
+		public string Name { get { return Name; } set { Name = value; }}
+		public string Race { get { return Race; } set { Race = value; }}
 
 		public Cat(int Age, double Height, double Weight, string Name, string Race)
-        {
+		{
 			this.Age = Age;
 			this.Height = Height;
 			this.Weight = Weight;
 			this.Name = Name;
 			this.Race = Race;
-        }
-
-    }
+		}
+	}
 }
