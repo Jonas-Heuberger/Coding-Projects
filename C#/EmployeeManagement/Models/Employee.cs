adc1173c2e72d1241cb3cedc798cf20434297ef4
Jonas Heuberger <90388219+Jonas-Heuberger@users.noreply.github.com>
2022-05-02 03:49:37 +0200

    working version of employee management project

---
+++ C#/EmployeeManagement/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace EmployeeManagement.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Prename { get; set; }
+        public string Lastname { get; set; }
+    }
+}
