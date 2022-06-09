2269d856ba00cfe19c861470af2fe609b9f23d8f
Jonas_Heuberger <90388219+Jonas-Heuberger@users.noreply.github.com>
2022-06-09 10:27:33 +0200

    Update SchuelerController.cs

--- C#/test/Controllers/SchuelerController.cs
+++ C#/test/Controllers/SchuelerController.cs
@@ -5,6 +5,7 @@ using test.Models;
 namespace test.Controllers
    
 {
+    [Authorize()]
     public class SchuelerController : ODataController
     {
         private DbApiContext _dbApiContext;
@@ -18,10 +19,25 @@ namespace test.Controllers
             return _dbApiContext.Schueler.AsQueryable();
         }
 
+        // post = create, patch = update (edit), delete = delete, get = read
         public SingleResult<Schueler> Get([FromODataUri] Guid key)
         {
             var result = _dbApiContext.Schueler.Where(s => s.rowguid == key);
             return SingleResult.Create(result);
         }
+
+        // public async Task<IActionResult> Delete([FromODataUri] Guid key)
+        // {
+        //   var schueler = await _dbApiContext.Schueler.FindAsync(key);
+        //   if (schueler == null)
+        //   {
+        //     return NotFound();
+        //   }
+        //   var sql = "exec dbo.usp_DeleteSchueler";
+
+        //   var result = _dbApiContext.Database.ExecuteSqlRaw(sql);
+
+        //   return Ok(result);
+        // }
 }
 }
