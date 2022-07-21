978d91dada4be8a05a39760ad5dea00df0b7f9a6
Jonas_Heuberger <90388219+Jonas-Heuberger@users.noreply.github.com>
2022-07-21 14:11:22 +0200

    played with printf() function

--- C/hello.c
+++ C/hello.c
@@ -1,74 +1,9 @@
 #include <stdio.h>
 
-int main() {
-  int num = inputNumber();
-  int cash = atm(num);
-  printf("%d", cash);
+int main(){
+char Hello[] = "Hello";
+char World[] = "World";
+   // printf("%s %s \n", Hello, World);
+   printf("%s \n", Hello);
   return 0;
 }
-
-// Language: c
-
-int inputNumber() {
-  int number;
-  printf("Enter a number: ");
-  scanf("%d", &number);
-  return number;
-}
-
-int *atm(int num) {
-  int cash[9];
-  for (int i = 0; i <= 100; i++) {
-    cash[1] = 1;
-  }
-  for (int i = 0; num <= 200; i++)
-  {
-    /* code */
-     cash[2] = i;
-  }
-
-  for (int i = 0; num <= 100; i++)
-  {
-    /* code */
-    cash[3] = i;
-  }
-
-  for (int i = 0; num <= 50; i++)
-  {
-    /* code */
-    cash[4] = i;
-  }
-
-  for (int i = 0; num <= 20; i++)
-  {
-    /* code */
-    cash[5] = i;
-  }
-
-  for (int i = 0; num <= 10; i++)
-  {
-    /* code */
-    cash[6] = i;
-  }
-
-  for (int i = 0; num <= 5; i++)
-  {
-    /* code */
-    cash[7] = i;
-  }
-
-  for (int i = 0; num <= 2; i++)
-  {
-    /* code */
-    cash[8] = i;
-  }
-
-  for (int i = 0; num <= 1; i++)
-  {
-    /* code */
-    cash[9] = i;
-  }
-  
-  return cash;
-}
-
