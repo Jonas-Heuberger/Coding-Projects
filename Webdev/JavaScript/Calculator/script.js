666ce3f85d695c90068148395b0aaddce178cc36
Jonas_Heuberger <90388219+Jonas-Heuberger@users.noreply.github.com>
2022-08-12 02:03:45 +0200

    finished the tutorial

---
+++ Webdev/JavaScript/Calculator/script.js
@@ -0,0 +1,16 @@
+function Plus(a, b) {
+    return a + b;
+}
+    
+function Minus(a, b) {
+    return a - b;
+}
+
+function Multiply(a, b) {
+    return a * b;
+}
+    
+function Divide(a, b) {
+    return a / b;
+}
+
