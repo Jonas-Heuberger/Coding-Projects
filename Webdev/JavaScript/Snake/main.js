926ba9ebdbe7b40f18c824db71f6e69bdf813593
Jonas_Heuberger <90388219+Jonas-Heuberger@users.noreply.github.com>
2022-08-24 07:59:34 +0200

    played with my code

--- Webdev/JavaScript/Snake/main.js
+++ Webdev/JavaScript/Snake/main.js
@@ -1,9 +1,33 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 720;
-canvas.height = 480;
+canvas.height = 720;
+canvas.rows = 20;
+canvas.columns = 20;
+canvas.cellWidth = canvas.width / canvas.columns - 1;
+canvas.cellHeight = canvas.height / canvas.rows - 1;
 
 const map = new Map();
 map.draw(ctx);
+
+const snake = new Snake();
+snake.position[{
+    x: 8,
+    y: 8
+}];
+
+snake.drawSnake(ctx);
+
+const food = new Food();
+food.position.x = 5;
+food.position.y = 5;
+
+food.draw(ctx);
+
+const controls = new Controls();
+
+function gameLoop() {
+   
+}
 
 
