6630aeb66279a622f7fb3f4ca0f31d2a730a3126
Jonas_Heuberger <90388219+Jonas-Heuberger@users.noreply.github.com>
2022-09-26 22:31:35 +0200

    Update database.sql

--- Databases/Cheatsheet/database.sql
+++ Databases/Cheatsheet/database.sql
@@ -1,7 +1,7 @@
 create database Schule
 go
 
-use Schule
+use dbo.Schule
 
 create table tbl_Klasse
 (
@@ -9,30 +9,10 @@ create table tbl_Klasse
     CONSTRAINT pk_tbl_Klasse PRIMARY KEY,
     CONSTRAINT DF_tbl_Klasse_rowguid DEFAULT newid(),
     Klassenname varchar(50) NOT NULL,
-    fk_Schueler_Id UNIQUEIDENTIFIER NOT NULL,
-    fk_Lehrer_Id UNIQUEIDENTIFIER NOT NULL
-        CONSTRAINT fk_tbl_Klasse_tbl_Schueler FOREIGN KEY (fk_Schueler_Id) REFERENCES tbl_Schueler (rowguid),
-    CONSTRAINT fk_tbl_Klasse_tbl_Lehrer FOREIGN KEY (fk_Lehrer_Id) REFERENCES tbl_Lehrer (rowguid)
+    
 
 )
 GO
-
-create table tbl_Schueler
-(
-    rowguid uniqueidentifier not null,
-    CONSTRAINT PK_tbl_Schueler PRIMARY KEY,
-    CONSTRAINT DF_tbl_Schueler_rowguid DEFAULT newid(),
-    Vorname nvarchar(50) not null,
-    Nachname nvarchar(50) not null,
-    Geburtsdatum date not null,
-    fk_KlasseID UNIQUEIDENTIFIER not null,
-    fk_FaecherID UNIQUEIDENTIFIER not null,
-    fk_LehrerID UNIQUEIDENTIFIER not null,
-    CONSTRAINT fk_tbl_Schueler_tbl_Klasse FOREIGN KEY (fk_KlasseID) REFERENCES tbl_Klasse (rowguid),
-    CONSTRAINT fk_tbl_Schueler_tbl_Faecher FOREIGN KEY (fk_FaecherID) REFERENCES tbl_Faecher (rowguid),
-    CONSTRAINT fk_tbl_Schueler_tbl_Lehrer FOREIGN KEY (fk_LehrerID) REFERENCES tbl_Lehrer (rowguid)
-)
-go
 
 create table tbl_Faecher
 (
@@ -40,10 +20,6 @@ create table tbl_Faecher
     CONSTRAINT PK_tbl_Faecher PRIMARY KEY,
     CONSTRAINT DF_tbl_Faecher_rowguid DEFAULT newid(),
     Fachname nvarchar(50) not null,
-    fk_SchuelerID UNIQUEIDENTIFIER not null,
-    fk_LehrerID UNIQUEIDENTIFIER not null,
-    CONSTRAINT fk_tbl_Faecher_tbl_Lehrer FOREIGN KEY (fk_LehrerID) REFERENCES tbl_Lehrer (rowguid),
-    CONSTRAINT fk_tbl_Faecher_tbl_Schueler FOREIGN KEY (fk_SchuelerID) REFERENCES tbl_Schueler (rowguid)
 )
 go
 
@@ -57,13 +33,27 @@ create table tbl_Lehrer
     Geburtsdatum date not null,
     fk_FaecherID UNIQUEIDENTIFIER not null,
     fk_KlasseID UNIQUEIDENTIFIER not null,
-    fk_SchuelerID UNIQUEIDENTIFIER not null,
     CONSTRAINT fk_tbl_Lehrer_tbl_Faecher FOREIGN KEY (fk_FaecherID) REFERENCES tbl_Faecher (rowguid),
     CONSTRAINT fk_tbl_Lehrer_tbl_Klasse FOREIGN KEY (fk_KlasseID) REFERENCES tbl_Klasse (rowguid),
-    CONSTRAINT fk_tbl_Lehrer_tbl_Schueler FOREIGN KEY (fk_SchuelerID) REFERENCES tbl_Schueler (rowguid)
 )
 go
 
+create table tbl_Schueler
+(
+    rowguid uniqueidentifier not null,
+    CONSTRAINT PK_tbl_Schueler PRIMARY KEY,
+    CONSTRAINT DF_tbl_Schueler_rowguid DEFAULT newid(),
+    Vorname nvarchar(50) not null,
+    Nachname nvarchar(50) not null,
+    Geburtsdatum date not null,
+    fk_KlasseID UNIQUEIDENTIFIER not null,
+    fk_LehrerID UNIQUEIDENTIFIER not null,
+    fk_FaecherID UNIQUEIDENTIFIER not null,
+    CONSTRAINT fk_tbl_Schueler_tbl_Klasse FOREIGN KEY (fk_KlasseID) REFERENCES tbl_Klasse (rowguid),
+    CONSTRAINT fk_tbl_Schueler_tbl_Lehrer FOREIGN KEY (fk_LehrerID) REFERENCES tbl_Lehrer (rowguid),
+    CONSTRAINT fk_tbl_Schueler_tbl_Faecher FOREIGN KEY (fk_FaecherID) REFERENCES tbl_Faecher (rowguid)
+)
+go
 
 
     
