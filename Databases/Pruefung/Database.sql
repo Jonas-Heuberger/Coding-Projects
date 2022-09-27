386e165f585379a3ef0eedfbc54dbe9e4a7e5588
Jonas_Heuberger <90388219+Jonas-Heuberger@users.noreply.github.com>
2022-09-27 16:44:26 +0200

    pruefung files

---
+++ Databases/Pruefung/Database.sql
@@ -0,0 +1,232 @@
+CREATE DATABASE Friseursalon
+GO
+
+USE Friseursalon
+GO
+
+CREATE TABLE tbl_Lieferfirma
+(
+    rowguid UNIQUEIDENTIFIER NOT NULL
+        CONSTRAINT PK_tbl_Lieferfirma PRIMARY KEY,
+    CONSTRAINT DF_tbl_Lieferfirma_rowguid DEFAULT (newid()),
+    Firmenname NVARCHAR(50) NOT NULL,
+    Artikel VARCHAR(50) NOT NULL,
+    Preis DECIMAL(10,2) NOT NULL,
+)
+GO
+
+CREATE TABLE tbl_Artikel
+(
+    rowguid UNIQUEIDENTIFIER NOT NULL
+        CONSTRAINT PK_tbl_Artikel PRIMARY KEY,
+    CONSTRAINT DF_tbl_Artikel_rowguid DEFAULT (newid()),
+    Artikelname NVARCHAR(50) NOT NULL,
+    Preis DECIMAL(10,2) NOT NULL,
+    Lieferfirma UNIQUEIDENTIFIER NOT NULL
+        CONSTRAINT FK_tbl_Artikel_tbl_Lieferfirma FOREIGN KEY (Lieferfirma) REFERENCES tbl_Lieferfirma(rowguid)
+)
+GO
+
+CREATE TABLE tbl_Abonnements
+(
+    rowguid UNIQUEIDENTIFIER NOT NULL
+        CONSTRAINT PK_tbl_Abonnements PRIMARY KEY,
+    CONSTRAINT DF_tbl_Abonnements_rowguid DEFAULT (newid()),
+    Abonnementname NVARCHAR(50) NOT NULL,
+    Preis DECIMAL(10,2) NOT NULL,
+    Dienstleistung UNIQUEIDENTIFIER NOT NULL
+        CONSTRAINT FK_tbl_Abonnements_tbl_Dienstleistungen FOREIGN KEY (Dienstleistung) REFERENCES tbl_Dienstleistungen(rowguid)
+)
+GO
+
+CREATE TABLE tbl_Dienstleistung
+(
+    rowguid UNIQUEIDENTIFIER NOT NULL
+        CONSTRAINT PK_tbl_Dienstleistung PRIMARY KEY,
+    CONSTRAINT DF_tbl_Dienstleistung_rowguid DEFAULT (newid()),
+    Dienstleistungname NVARCHAR(50) NOT NULL,
+    Preis DECIMAL(10,2) NOT NULL,
+)
+GO
+
+CREATE TABLE tbl_Mitarbeiter
+(
+    rowguid UNIQUEIDENTIFIER NOT NULL
+        CONSTRAINT PK_tbl_Mitarbeiter PRIMARY KEY,
+    CONSTRAINT DF_tbl_Mitarbeiter_rowguid DEFAULT (newid()),
+    Vorname NVARCHAR(50) NOT NULL,
+    Nachname NVARCHAR(50) NOT NULL,
+    Aufgabe NVARCHAR(50) NOT NULL,
+    Anwesenheitsdaten DATE NOT NULL,
+
+)
+GO
+
+CREATE TABLE tbl_Kunden
+(
+    rowguid UNIQUEIDENTIFIER NOT NULL
+        CONSTRAINT PK_tbl_Kunden PRIMARY KEY,
+    CONSTRAINT DF_tbl_Kunden_rowguid DEFAULT (newid()),
+    Vorname NVARCHAR(50) NOT NULL,
+    Nachname NVARCHAR(50) NOT NULL,
+    Telefonnummer NVARCHAR(50) NOT NULL,
+    EMail NVARCHAR(50) NOT NULL,
+    Geburtsdatum DATE NOT NULL,
+    Adresse NVARCHAR(50) NOT NULL,
+    PLZ NVARCHAR(50) NOT NULL,
+    Ort NVARCHAR(50) NOT NULL,
+    StylistID UNIQUEIDENTIFIER NOT NULL
+        CONSTRAINT FK_tbl_Kunden_tbl_Mitarbeiter FOREIGN KEY (StylistID) REFERENCES tbl_Mitarbeiter(rowguid)
+)
+GO
+
+CREATE TABLE tbl_Chemie
+(
+    rowguid UNIQUEIDENTIFIER NOT NULL
+        CONSTRAINT PK_tbl_Chemie PRIMARY KEY,
+    CONSTRAINT DF_tbl_Chemie_rowguid DEFAULT (newid()),
+    ChemieName NVARCHAR(50) NOT NULL,
+    Preis DECIMAL(10,2) NOT NULL,
+)
+GO
+
+CREATE TABLE tbl_Ausprägung
+(
+    rowguid UNIQUEIDENTIFIER NOT NULL
+        CONSTRAINT PK_tbl_Ausprägung PRIMARY KEY,
+    CONSTRAINT DF_tbl_Ausprägung_rowguid DEFAULT (newid()),
+    AusprägungName NVARCHAR(50) NOT NULL,
+    Preis DECIMAL(10,2) NOT NULL,
+)
+GO
+
+CREATE TABLE tbl_Anwesenheit
+(
+    rowguid UNIQUEIDENTIFIER NOT NULL
+        CONSTRAINT PK_tbl_Termin PRIMARY KEY,
+    CONSTRAINT DF_tbl_Termin_rowguid DEFAULT (newid()),
+    Datum DATE NOT NULL,
+    Uhrzeit TIME NOT NULL,
+    Dauer TIME NOT NULL,
+)
+GO
+
+CREATE TABLE tbl_Assistent
+(
+    rowguid UNIQUEIDENTIFIER PRIMARY KEY NOT NULL
+        FOREIGN KEY (rowguid) REFERENCES tbl_Mitarbeiter(rowguid),
+    Vorname NVARCHAR(50) NOT NULL
+        FOREIGN KEY (Vorname) REFERENCES tbl_Mitarbeiter(Vorname),
+    Nachname NVARCHAR(50) NOT NULL
+        FOREIGN KEY (Nachname) REFERENCES tbl_Mitarbeiter(Nachname),
+    Aufgabe NVARCHAR(50) NOT NULL
+        FOREIGN KEY (Aufgabe) REFERENCES tbl_Mitarbeiter(Aufgabe),
+    Anwesenheitsdaten DATE NOT NULL
+        FOREIGN KEY (Anwesenheitsdaten) REFERENCES tbl_Mitarbeiter(Anwesenheitsdaten),
+)
+GO
+
+CREATE TABLE tbL_Stylist
+(
+    rowguid UNIQUEIDENTIFIER PRIMARY KEY NOT NULL
+        FOREIGN KEY (rowguid) REFERENCES tbl_Mitarbeiter(rowguid),
+    Vorname NVARCHAR(50) NOT NULL
+        FOREIGN KEY (Vorname) REFERENCES tbl_Mitarbeiter(Vorname),
+    Nachname NVARCHAR(50) NOT NULL
+        FOREIGN KEY (Nachname) REFERENCES tbl_Mitarbeiter(Nachname),
+    Aufgabe NVARCHAR(50) NOT NULL
+        FOREIGN KEY (Aufgabe) REFERENCES tbl_Mitarbeiter(Aufgabe),
+    Anwesenheitsdaten DATE NOT NULL,
+    FOREIGN KEY (Anwesenheitsdaten) REFERENCES tbl_Mitarbeiter(Anwesenheitsdaten),
+)
+GO
+
+CREATE TABLE tbl_Kunde_kauft_Artikel
+(
+    rowguid UNIQUEIDENTIFIER NOT NULL
+        CONSTRAINT PK_tbl_Kunde_kauft_Artikel PRIMARY KEY
+        CONSTRAINT DF_tbl_Kunde_kauft_Artikel_rowguid DEFAULT (newid()),
+    Kunde UNIQUEIDENTIFIER NOT NULL
+        CONSTRAINT FK_tbl_Kunde_kauft_Artikel_tbl_Kunden FOREIGN KEY (Kunde) REFERENCES tbl_Kunden(rowguid),
+    Artikel UNIQUEIDENTIFIER NOT NULL
+        CONSTRAINT FK_tbl_Kunde_kauft_Artikel_tbl_Artikel FOREIGN KEY (Artikel) REFERENCES tbl_Artikel(rowguid),
+)
+GO
+
+CREATE TABLE tbl_Kunde_bezieht_tbl_Dienstleistung_durch_tbl_Stylist
+(
+    rowguid UNIQUEIDENTIFIER NOT NULL
+        CONSTRAINT PK_tbl_Kunde_bezieht_tbl_Dienstleistung PRIMARY KEY
+        CONSTRAINT DF_tbl_Kunde_bezieht_tbl_Dienstleistung_rowguid DEFAULT (newid()),
+    Kunde UNIQUEIDENTIFIER NOT NULL
+        CONSTRAINT FK_tbl_Kunde_bezieht_tbl_Dienstleistung_tbl_Kunden FOREIGN KEY (Kunde) REFERENCES tbl_Kunden(rowguid),
+    Dienstleistung UNIQUEIDENTIFIER NOT NULL
+        CONSTRAINT FK_tbl_Kunde_bezieht_tbl_Dienstleistung_tbl_Dienstleistungen FOREIGN KEY (Dienstleistung) REFERENCES tbl_Dienstleistungen(rowguid),
+    Stylist UNIQUEIDENTIFIER NOT NULL
+        CONSTRAINT FK_tbl_Kunde_bezieht_tbl_Dienstleistung_tbl_Stylist FOREIGN KEY (Stylist) REFERENCES tbl_Stylist(rowguid),
+)
+GO
+
+CREATE TABLE tbl_Kunde_erwirbt_tbl_Abonnements
+(
+    rowguid UNIQUEIDENTIFIER NOT NULL
+        CONSTRAINT PK_tbl_Kunde_erwirbt_tbl_Abonnements PRIMARY KEY
+        CONSTRAINT DF_tbl_Kunde_erwirbt_tbl_Abonnements_rowguid DEFAULT (newid()),
+    Kunde UNIQUEIDENTIFIER NOT NULL
+        CONSTRAINT FK_tbl_Kunde_erwirbt_tbl_Abonnements_tbl_Kunden FOREIGN KEY (Kunde) REFERENCES tbl_Kunden(rowguid),
+    Abonnement UNIQUEIDENTIFIER NOT NULL
+        CONSTRAINT FK_tbl_Kunde_erwirbt_tbl_Abonnements_tbl_Abonnements FOREIGN KEY (Abonnement) REFERENCES tbl_Abonnements(rowguid),
+)
+GO
+
+CREATE TABLE tbl_Stylist_hat_tbl_Assistent
+(
+    rowguid UNIQUEIDENTIFIER NOT NULL
+        CONSTRAINT PK_tbl_Stylist_hat_tbl_Assistent PRIMARY KEY
+        CONSTRAINT DF_tbl_Stylist_hat_tbl_Assistent_rowguid DEFAULT (newid()),
+    Stylist UNIQUEIDENTIFIER NOT NULL
+        CONSTRAINT FK_tbl_Stylist_hat_tbl_Assistent_tbl_Stylist FOREIGN KEY (Stylist) REFERENCES tbl_Stylist(rowguid),
+    Assistent UNIQUEIDENTIFIER NOT NULL
+        CONSTRAINT FK_tbl_Stylist_hat_tbl_Assistent_tbl_Assistent FOREIGN KEY (Assistent) REFERENCES tbl_Assistent(rowguid),
+)
+GO
+
+CREATE TABLE tbl_Mitarbeiter_haben_tbl_Anwesenheit
+(
+    rowguid UNIQUEIDENTIFIER NOT NULL
+        CONSTRAINT PK_tbl_Mitarbeiter_haben_tbl_Anwesenheit PRIMARY KEY
+        CONSTRAINT DF_tbl_Mitarbeiter_haben_tbl_Anwesenheit_rowguid DEFAULT (newid()),
+    Mitarbeiter UNIQUEIDENTIFIER NOT NULL
+        CONSTRAINT FK_tbl_Mitarbeiter_haben_tbl_Anwesenheit_tbl_Mitarbeiter FOREIGN KEY (Mitarbeiter) REFERENCES tbl_Mitarbeiter(rowguid),
+    Anwesenheit UNIQUEIDENTIFIER NOT NULL
+        CONSTRAINT FK_tbl_Mitarbeiter_haben_tbl_Anwesenheit_tbl_Anwesenheit FOREIGN KEY (Anwesenheit) REFERENCES tbl_Anwesenheit(rowguid),
+)
+GO
+
+CREATE TABLE tbl_Dienstleistung_braucht_tbl_Chemie
+(
+    rowguid UNIQUEIDENTIFIER NOT NULL
+        CONSTRAINT PK_tbl_Dienstleistung_braucht_tbl_Chemie PRIMARY KEY
+        CONSTRAINT DF_tbl_Dienstleistung_braucht_tbl_Chemie_rowguid DEFAULT (newid()),
+    Dienstleistung UNIQUEIDENTIFIER NOT NULL
+        CONSTRAINT FK_tbl_Dienstleistung_braucht_tbl_Chemie_tbl_Dienstleistungen FOREIGN KEY (Dienstleistung) REFERENCES tbl_Dienstleistungen(rowguid),
+    Chemie UNIQUEIDENTIFIER NOT NULL
+        CONSTRAINT FK_tbl_Dienstleistung_braucht_tbl_Chemie_tbl_Chemie FOREIGN KEY (Chemie) REFERENCES tbl_Chemie(rowguid),
+)
+GO
+
+CREATE TABLE tbl_Dienstleistung_hat_Ausprägung
+(
+    rowguid UNIQUEIDENTIFIER NOT NULL
+        CONSTRAINT PK_tbl_Dienstleistung_hat_Ausprägung PRIMARY KEY
+        CONSTRAINT DF_tbl_Dienstleistung_hat_Ausprägung_rowguid DEFAULT (newid()),
+    Dienstleistung UNIQUEIDENTIFIER NOT NULL
+        CONSTRAINT FK_tbl_Dienstleistung_hat_Ausprägung_tbl_Dienstleistungen FOREIGN KEY (Dienstleistung) REFERENCES tbl_Dienstleistungen(rowguid),
+    Ausprägung UNIQUEIDENTIFIER NOT NULL
+        CONSTRAINT FK_tbl_Dienstleistung_hat_Ausprägung_tbl_Ausprägung FOREIGN KEY (Ausprägung) REFERENCES tbl_Ausprägung(rowguid),
+)
+GO
+
+
+
+
